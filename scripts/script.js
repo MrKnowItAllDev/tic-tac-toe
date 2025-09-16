cd9a74316311cc6e5d9c520f5374d9a4eb11e939
Nathan <nathanbrentlloyd2000@gmail.com>
2025-09-16 16:03:49 +0000

    Game logic, html code, css styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,140 @@
 'use strict';
 
-function gameBoard() {
-    const board = [];
-    for (let i = 0; i < 3 * 3; i++) {
-        board.push([]);
+const gameBoard = (() => {
+    let board = Array(9).fill(null);
+
+    const isTaken = (pos) => board[pos] !== null;
+    const getAvailable = () => board.filter((pos) => !pos);
+    const getBoard = () => board;
+    const placeMove = (mark, pos) => board[pos] = mark;
+    const setBoard = () => board = Array(9).fill(null);
+
+    const getWinner = (move) => {
+        const wPositions = [
+            [0, 1, 2], [0, 4, 8], [0, 3, 6],
+            [1, 4, 7], [2, 4, 6], [3, 4, 5], [6, 7, 8], [2, 5, 8]
+        ];
+
+        return wPositions.map((arr) => {
+            return arr.filter((pos) => {
+                return board[pos] === move;
+            })
+        }).filter((a) => a.length === 3)[0];
     }
-    return board;
+
+    const getTie = () => {
+        return board.filter((p) => p === null).length === 1;
+    }
+
+    return {
+        isTaken, getAvailable, getTie,
+        placeMove, getBoard, getWinner,
+        setBoard
+    };
+})();
+
+function Player(name, mark) {
+    const pMark = mark;
+    let score = 0;
+
+    const getScore = () => score;
+    const incScore = () => score++;
+    const getMove = () => pMark;
+    const setName = (pName) => name = pName;
+    const getName = () => name;
+
+    return { getMove, getScore, incScore, getName, setName }
 }
 
-function Player(player, mark) {
-    this.player = player;
-    this.mark = mark;
+function gameController() {
+    const board = gameBoard;
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+
+    let activePlayer = player1;
+    const uiInfo = {};
+
+    const getBoard = () => board.getBoard();
+    const getTie = () => board.getTie();
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const validPos = (pos) => {
+        return pos >= 0 && pos < getBoard().length;
+    }
+
+    const playGame = (pos) => {
+        if (!validPos(pos)) {
+            uiInfo['invalid'] = `Please select a valid position`;
+            return;
+        }
+        if (!board.getWinner(player1.getMove()) && !board.getWinner(player2.getMove()))
+            if (!board.isTaken(pos) && validPos(pos)) {
+                board.placeMove(activePlayer.getMove(), pos);
+                uiInfo['activePlayer'] = activePlayer;
+                switchPlayer();
+            }
+            else uiInfo['occupiedMsg'] = `This position is occupied`; // Element textContent update
+        if (board.getWinner(player1.getMove()) || board.getWinner(player2.getMove())) {
+            uiInfo['winner'] = board.getWinner(player1.getMove()) ? player1 : player2;
+        }
+    }
+
+    return { playGame, getActivePlayer, board, getBoard, getTie, uiInfo }
 }
 
 function displayController() {
+    const game = gameController();
+    const board = game.getBoard();
 
+    const grid = document.querySelector('#grid');
+    const player1 = document.querySelector('.player-1');
+    const player2 = document.querySelector('.player-2');
+    const playerOutput = document.querySelector('.announce');
+
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-item');
+        grid.appendChild(cell);
+    }
+
+    const reset = () => {
+        playerOutput.textContent = ``;
+        player1.textContent = `Player 1: `;
+        player2.textContent = `Player 2: `;
+        const cells = document.querySelectorAll('.grid-item');
+        cells.forEach((cell) => {
+            cell.textContent = ``;
+        });
+    }
+
+    const gameRules = (event, pos) => {
+        if (!game.uiInfo['winner']) {
+            event.target.textContent = game.getActivePlayer().getMove();
+            game.playGame(pos);
+        }
+    }
+
+    const playRound = () => {
+        const cells = document.querySelectorAll('.grid-item');
+        cells.forEach((cell, i) => {
+            cell.addEventListener('click', (e) => {
+                gameRules(e, i);
+                if (game.uiInfo['winner']) {
+                    let player = game.uiInfo['winner'];
+                    player.incScore();
+                    document.querySelector('.announce').textContent = `${player.getName()} Wins!`;
+                    document.querySelector('.player-1').textContent = `Player 1: ${player.getScore()}`;
+                }
+            });
+        });
+    }
+    return { playRound }
 }
+
+const main = displayController();
+main.playRound();
